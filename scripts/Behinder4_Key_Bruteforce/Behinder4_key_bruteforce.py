c3c78284e476e6cb3195e962a371325e046a82d8
Threekiii <472361400@qq.com>
2023-12-09 13:55:56 +0800

    update cheatsheets

--- scripts/Behinder4_Key_Bruteforce/Behinder4_key_bruteforce.py
+++ scripts/Behinder4_Key_Bruteforce/Behinder4_key_bruteforce.py
@@ -1,49 +1,49 @@
 # -*- coding: utf-8 -*-
 # @Author  : Threekiii
 # @Time    : 2023/11/29 18:07
-# @Function: Brute force of Behinder4 secret key
+
 
 import base64
 import hashlib
-from Crypto.Cipher import AES
+
 
 
 def aes_decode(data, key):
-    try:
+        aes = AES.new(str.encode(key), AES.MODE_ECB)
         aes = AES.new(str.encode(key), AES.MODE_ECB)
         decrypted_text = aes.decrypt(data)
         decrypted_text = decrypted_text[:-(decrypted_text[-1])]
     except Exception as e:
-        print(e)
     else:
-        return decrypted_text.decode()
+    else:
 
 def base64_decode(data):
+def base64_decode(data):
     res = base64.b64decode(data.strip()).decode()
-    print(res)
+    return res
     return res
 
 def md5_truncate(key):
     return hashlib.md5(key.encode()).hexdigest()[:16]
-
+if __name__ == '__main__':
 if __name__ == '__main__':
     data = '''<BASE64_ENCODED_ENCRYPTED_DATA_HERE>'''
-    with open('keys.txt','r',encoding='utf-8') as f:
         keys = f.readlines()
-
+        keys = f.readlines()
     for key in keys:
-        key = key.strip()
+    for key in keys:
         c2_key = md5_truncate(key)
-        print('[CURRENT KEY]\t{} {}'.format(key,c2_key))
+        c2_key = md5_truncate(key)
         try:
-            data_b64_decode = base64.b64decode(data.strip())
+        try:
             data_aes_decode = aes_decode(data_b64_decode, c2_key)
             if data_aes_decode:
+            if data_aes_decode:
                 print('[Ooooops, We found it!]')
-                print(data_aes_decode)
+                break
                 break
         except:
-            pass
+
 
 
 
